d20376f3cd7379b17205282bf1ec839443c334ff
Karen96Aldana13 <135787750+Karen96Aldana13@users.noreply.github.com>
2023-06-22 19:36:33 -0500

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -3,11 +3,11 @@
     function Getconexion()
     {
 
-    $conexion = new mysqli ("localhost", "root", "123456", "empresa");
+    $conexion = new mysqli ("localhost", "root", "123456", "empresa")
 
     if ($conexion-> connect_errno)
     {
-        echo "Error al conectarse a la base de datos".$conexion->connect_error;
+        echo "Error al conectarse a la base de datos".$conexion->connect_error
     }
     else
     {
